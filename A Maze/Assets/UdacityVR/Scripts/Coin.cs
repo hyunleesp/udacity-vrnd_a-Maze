2bd8ad7a465c9a0046041852c65b003b624fb9ec
hyun ji lee <hyun_.lee@outlook.com>
2018-12-18 11:17:31 -0200

    commit project folder A Maze

---
+++ A Maze/Assets/UdacityVR/Scripts/Coin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Coin : MonoBehaviour {
+
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
+    public GameObject coinPrefab;
+    public GameObject coinPoofPrefab;
+	void Update () {
+        // OPTIONAL-CHALLENGE: Animate the coin rotating
+        // TIP: You could use a method from the Transform class
+        coinPrefab.transform.Rotate(0f, 5.0f, 0f);
+    }
+
+
+	public void OnCoinClicked () {
+        /// Called when the 'Coin' game object is clicked
+        /// - Displays a poof effect (handled by the 'CoinPoof' prefab)
+        /// - Plays an audio clip (handled by the 'CoinPoof' prefab)
+        /// - Removes the coin from the scene
+        /// Prints to the console when the method is called
+        Debug.Log ("'Coin.OnCoinClicked()' was called");
+        
+        // TODO: Display the poof effect and remove the coin from the scene
+        // Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
+        Instantiate(coinPoofPrefab, transform.position, Quaternion.Euler(-90, 0, 0));
+        // Use Destroy() to delete the coin after for example 0.5 seconds
+        Destroy(coinPrefab, 0.5f);
+    }
+}
---
+++ A Maze/Assets/UdacityVR/Scripts/Key.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Key : MonoBehaviour {
+
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
+    public GameObject keyPoofPrefab;
+    public GameObject keyPrefab;
+    // Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+    public Door door;
+
+	void Update () {
+        // OPTIONAL-CHALLENGE: Animate the key rotating
+        // TIP: You could use a method from the Transform class
+        keyPrefab.transform.Rotate(0f, 0f, 5.0f);
+	}
+
+
+	public void OnKeyClicked () {
+		/// Called when the 'Key' game object is clicked
+		/// - Unlocks the door (handled by the Door class)
+		/// - Displays a poof effect (handled by the 'KeyPoof' prefab)
+		/// - Plays an audio clip (handled by the 'KeyPoof' prefab)
+		/// - Removes the key from the scene
+
+		// Prints to the console when the method is called
+		Debug.Log ("'Key.OnKeyClicked()' was called");
+        // TODO: Unlock the door, display the poof effect, and remove the key from the scene
+        // Use 'door' to call the Door.Unlock() method
+        door.Unlock();
+        //door.Unlock;
+        // Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
+        Instantiate(keyPoofPrefab, transform.position, Quaternion.Euler(-90, 0, 0));
+        // Use Destroy() to delete the key after for example 0.5 seconds
+        Destroy(keyPrefab, 0.5f);
+	}
+}
